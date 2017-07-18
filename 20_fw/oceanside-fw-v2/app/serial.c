72ad48630935d38a53d693bc1563800053ceee8b
bus710 <bus710@gmail.com>
2017-07-18 15:51:19 -0700

    .

--- 20_fw/oceanside-fw-v2/app/serial.c
+++ 20_fw/oceanside-fw-v2/app/serial.c
@@ -1,11 +1,16 @@
+#include <stdbool.h>
 #include "ch.h"
 #include "hal.h"
+#include "serial.h"
 
+
+volatile int tx_done;
 
 static THD_WORKING_AREA(waThread_serial, 128);
 
 static void txend1(UARTDriver *uartp){
   (void)uartp;
+	tx_done = true;
 }
 
 static void txend2(UARTDriver *uartp){
@@ -38,6 +43,16 @@ static UARTConfig uart_cfg_1 = {
   0
 };
 
+static void message_init(MESSAGE* msg){
+	msg->preamble_0 = 0xaa;
+	msg->preamble_1 = 0xaa;
+	msg->command_0 = 0x00;
+	msg->command_1 = 0x00;
+	msg->len = 0;
+	for (int i=0; i<32; i++){
+		msg->pl[i] = 0x00;
+	}
+}
 
 static THD_FUNCTION(Thread_serial, arg) {
 
@@ -51,9 +66,16 @@ static THD_FUNCTION(Thread_serial, arg) {
 
   char message = 'a';
 
+
 	while (true) {
+		MESSAGE msg;
+		message_init(&msg);
+
 		uartStartSend(&UARTD1, 1, &message);
-		chThdSleepMilliseconds(500);
+		uartStartSend(&UARTD1, 38, &msg);
+		if(tx_done == false){
+			chThdSleepMilliseconds(10);
+		}
 	}
 }
 
--- 20_fw/oceanside-fw-v2/app/serial.h
+++ 20_fw/oceanside-fw-v2/app/serial.h
@@ -1,8 +1,15 @@
-
-
-
 #ifndef SERIAL_H_
 #define SERIAL_H_
+
+typedef struct{
+	uint8_t preamble_0;
+	uint8_t preamble_1;
+	uint8_t command_0;
+	uint8_t command_1;
+	uint8_t len;
+	uint8_t pl[32];
+	uint8_t checksum;
+}MESSAGE;
 
 void serial_init(void);
 
